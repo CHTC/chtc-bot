94983bc9c3612e12c709f22fccc6e42ebc7dfd1d
Josh Karpel <karpel@wisc.edu>
2020-04-13 16:46:45 -0500

    move I'm alive message to the right place, maybe

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from bot import app, SLACK_CLIENT
 
+# TODO: discover channel by name
+SLACK_CLIENT.api_call(
+    "chat.postMessage", channel = "G011PN92WTV", text = "I'm alive!",
+)
+print("I sent the I'm alive message")
+
 if __name__ == "__main__":
-    # TODO: discover channel by name
-    SLACK_CLIENT.api_call(
-        "chat.postMessage", channel = "G011PN92WTV", text = "I'm alive!",
-    )
-    print('hi')
     app.run()
