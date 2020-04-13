191a462822253a7198029816ec9a139affdb5ea6
Josh Karpel <karpel@wisc.edu>
2020-04-13 16:36:25 -0500

    send a message when the bot wakes up

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from bot import app
+from bot import app, SLACK_CLIENT
 
 if __name__ == "__main__":
+    # TODO: discover channel by name
+    SLACK_CLIENT.api_call(
+        "chat.postMessage", channel = "G011PN92WTV", text = "I'm alive!",
+    )
     app.run()
