acf3958bb36d56f3d35f99003c7abe123595a960
Josh Karpel <karpel@wisc.edu>
2020-04-17 16:53:48 -0500

    clobber all newlines in keys and values in expressions report... this is certainly wrong, because the values might contain real newlines, but it's what I've got right now

--- web/slashes/classad_eval.py
+++ web/slashes/classad_eval.py
@@ -24,29 +24,38 @@ def handle_classad_eval():
 
 
 def classad_eval_reply(client, channel, user, text):
-    try:
-        ad, exprs = parse(text)
-        results = evaluate(ad, exprs)
-
-        prefix = f"<@{user}> asked me to evaluate {'a ' if len(exprs) == 1 else ''}ClassAd expression{formatting.plural(exprs)}"
-        if len(ad) != 0:
-            msg_lines = [
-                f"{prefix} in the context of this ad:",
-                "```",
-                *textwrap.dedent(str(ad)).strip().splitlines(),
-                "```",
-                f"Expressions:",
-            ]
-        else:
-            msg_lines = [f"{prefix}:"]
-
-        msg_lines.extend([*[f"`{k}` :arrow_right: `{v}`" for k, v in results.items()]])
-
-        msg = "\n".join(filter(None, msg_lines))
-    except Exception as e:
-        msg = f"Failed to parse ad or expressions: {e}"
+    msg = generate_classad_eval_reply(user, text)
 
     slack.post_message(client, channel=channel, text=msg)
+
+
+def generate_classad_eval_reply(user, text):
+    try:
+        ad, exprs = parse(text)
+    except Exception as e:
+        return f"Failed to parse ad or expressions: {e}"
+
+    try:
+        results = evaluate(ad, exprs)
+    except Exception as e:
+        return f"Failed to evaluate an expression: {e}"
+
+    prefix = f"<@{user}> asked me to evaluate {'a ' if len(exprs) == 1 else ''}ClassAd expression{formatting.plural(exprs)}"
+    if len(ad) != 0:
+        msg_lines = [
+            f"{prefix} in the context of this ad:",
+            "```",
+            *textwrap.dedent(str(ad)).strip().splitlines(),
+            "```",
+            f"Expressions:",
+        ]
+    else:
+        msg_lines = [f"{prefix}:"]
+
+    formatted_results = {str(k).replace('\n', ' ') : str(v).replace('\n', ' ') for k, v in results}
+    msg_lines.extend([*[f"`{repr(k)}` :arrow_right: `{v}`" for k, v in formatted_results.items()]])
+
+    return "\n".join(filter(None, msg_lines))
 
 
 RE_PARTS = re.compile(r"'(.*?)'")
