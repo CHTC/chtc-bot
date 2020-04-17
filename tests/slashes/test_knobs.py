a5ecf2cdddf86dea8d2c85923548c29678be4a7e
Josh Karpel <karpel@wisc.edu>
2020-04-17 12:14:30 -0500

    merge html-to-markdown tests

--- tests/slashes/test_knobs.py
+++ tests/slashes/test_knobs.py
@@ -12,19 +12,23 @@ def test_get_knob_description_returns_none_if_it_fails_to_find_the_knob():
     )
 
 
-def test_convert_em_to_underscores():
+@pytest.mark.parametrize(
+    "converter, html, expected",
+    [
+        (
+            knobs.convert_em_to_underscores,
+            "<em>this</em> <em>many_many</em> <em>condor_daemon_name</em>",
+            "_this_ _many_many_ _condor_daemon_name_",
+        ),
+        (
+            knobs.convert_code_to_backticks,
+            '<code class="docutils literal notranslate"><span class="pre">MASTER_NAME</span></code>',
+            "`MASTER_NAME`",
+        ),
+    ],
+)
+def test_convert_html_to_markdown(converter, html, expected):
     # This could include nested tags, but we're not testing BeautfulSoup here.
-    test_input = "<em>this</em> <em>many_many</em> <em>condor_daemon_name</em>"
-    soup = bs4.BeautifulSoup(test_input, "html.parser")
-    knobs.convert_em_to_underscores(soup)
-    test_output = soup.text
-    assert test_output == "_this_ _many_many_ _condor_daemon_name_"
-
-
-# My code-reuse detector is going off.
-def test_convert_code_to_backticks():
-    test_input = """<code class="docutils literal notranslate"><span class="pre">MASTER_NAME</span></code>"""
-    soup = bs4.BeautifulSoup(test_input, "html.parser")
-    knobs.convert_code_to_backticks(soup)
-    test_output = soup.text
-    assert test_output == "`MASTER_NAME`"
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    converter(soup)
+    assert soup.text == expected
