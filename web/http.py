eaccd00b3a0c2e7fdc626a9c527d9ba43ecc5463
Todd L Miller <tlmiller@cs.wisc.edu>
2020-04-19 22:43:53 -0500

    (#16)  All of our webpages having the encoding mis-detected, which is the reason for some the garbage characters on output.  UTF-8 should be correct for all of them, and I've verified that it is for the manual.

--- web/http.py
+++ web/http.py
@@ -5,4 +5,6 @@ import requests
 
 @functools.lru_cache(2 ** 6)
 def cached_get_url(url, *args, **kwargs):
-    return requests.get(url, *args, **kwargs)
+    r = requests.get(url, *args, **kwargs)
+    r.encoding = "utf-8"
+    return r
