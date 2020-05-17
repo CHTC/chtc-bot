429f36a18fa2949d39c4d83d2e31405a5ef04329
Todd L Miller <tlmiller@cs.wisc.edu>
2020-05-17 15:22:38 -0500

    No, seriously: why does the name change from 'description' in the RSS XML to 'summary' in the JSON after the Lambda function sends it?

--- tests/test_rss.py
+++ tests/test_rss.py
@@ -5,7 +5,7 @@ from web import rss
 @pytest.mark.parametrize("input, expected, rssCH", [
         (
             { 'title': 'Check-in [59661]:   (By Tim Theisen )',
-              'description': '(<span class="ticket"><a href="https://htcondor-wiki.cs.wisc.edu/index.cgi/tktview?tn=7643" class="new" title="Get HTCondor to build on Modern Fedora without warnings">#7643</a></span>) Fix compiler warning where strncpy count depends on source <br>',
+              'summary': '(<span class="ticket"><a href="https://htcondor-wiki.cs.wisc.edu/index.cgi/tktview?tn=7643" class="new" title="Get HTCondor to build on Modern Fedora without warnings">#7643</a></span>) Fix compiler warning where strncpy count depends on source <br>',
               'link': 'https://htcondor-wiki.cs.wisc.edu/index.cgi/chngview?cn=59661',
             },
             '<https://htcondor-wiki.cs.wisc.edu/index.cgi/chngview?cn=59661|Check-in [59661]:   (By Tim Theisen )>\n(<https://htcondor-wiki.cs.wisc.edu/index.cgi/tktview?tn=7643|#7643>) Fix compiler warning where strncpy count depends on source',
