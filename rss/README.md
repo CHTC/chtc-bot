1e2bd50ce2bcdab1bb4e847f3f90e527f1bb4912
Todd L Miller <tlmiller@cs.wisc.edu>
2020-05-18 09:08:58 -0500

    Rename variables to make them less meaningless.  Refactor DB access behind tiny little functions to make the code less obscure.  Format the code properly.  Pull the bot's RSS "API" endpoint from the Lambda function's environment.

--- rss/README.md
+++ rss/README.md
@@ -5,7 +5,10 @@
 configured.
 
 2.  Then go to the DynamoDB web console and create an entry in the
-`htcondor-wiki-rss-feed-reader-rssstorage-*` table.  Create new item
-whose `id` is `lambda.py`.  To that item, add `ts` (a float in string
-format), which is when you want to start updating from; and `ss`,
-the shared secret you added to the environment in Heroku;
+`htcondor-wiki-rss-feed-reader-rssstorage-*` table.  Create a new item
+whose `id` is `lambda.py`.  To that item, add `last_update_seen`
+(a float in string format; e.g., `date +%s -d "2020-05-18 00:00:00"`),
+past which events will be considered new; and `rss_shared_secret`,
+the shared secret you added to the environment (as `RSS_SHARED_SECRET`)
+in Heroku.
+
--- rss/lambda/lambda.py
+++ rss/lambda/lambda.py
@@ -5,63 +5,81 @@ import json
 import botocore, boto3
 
 # This is stupid, but you can't explicitly import module dependencies.
-sys.path.append('./libs')
+sys.path.append("./libs")
 
 import feedparser
 import requests
 
 
-def handler(event, context):
-    db = boto3.resource(
+def get_dynamo_db_table_from_environment():
+    return boto3.resource(
         "dynamodb",
         region_name=os.environ["dynamo_region"],
         config=botocore.client.Config(),
     ).Table(os.environ["dynamo_table"])
 
-    response = db.query(
-        KeyConditionExpression="id = :id",
-        ExpressionAttributeValues={":id": "lambda.py"},
+
+def get_item_from_table(table, key):
+    response = table.query(
+        KeyConditionExpression="id = :id", ExpressionAttributeValues={":id": key},
     )
     if len(response["Items"]) == 0:
-        return { "statusCode": 500, "body": json.dumps(f"database not configured"), }
+        return None
+    else:
+        return response["Items"][0]
 
-    firstResponse = response["Items"][0]
-    lastUpdateSeen = float(firstResponse["ts"])
-    sharedSecret = firstResponse["ss"]
+
+def set_attribute_of_item(table, key, attribute, value):
+    return table.update_item(
+        Key={"id": key},
+        UpdateExpression=f"set {attribute} = :attribute",
+        ExpressionAttributeValues={":attribute": value},
+        ReturnValues="UPDATED_NEW",
+    )
+
+
+def handler(event, context):
+    table = get_dynamo_db_table_from_environment()
+    state = get_item_from_table(table, "lambda.py")
+    if state is None:
+        return {
+            "statusCode": 500,
+            "body": json.dumps(f"database not configured"),
+        }
+
+    last_update_seen = float(state["last_update_seen"])
+    rss_shared_secret = state["rss_shared_secret"]
 
     d = feedparser.parse(
         "https://htcondor-wiki.cs.wisc.edu/index.cgi/timeline.rss?d=90"
     )
-    lastUpdate = time.mktime(d.feed.updated_parsed)
+    last_update = time.mktime(d.feed.updated_parsed)
 
-    if lastUpdateSeen < lastUpdate:
-        db.update_item(
-            Key={"id": "lambda.py"},
-            UpdateExpression="set ts = :ts",
-            ExpressionAttributeValues={":ts": str(lastUpdate)},
-            ReturnValues="UPDATED_NEW",
-        )
-
+    if last_update_seen < last_update:
+        set_attribute_of_item(table, "lambda.py", "last_update_seen", str(last_update))
         newEntries = []
         for entry in d.entries:
             published = time.mktime(entry.published_parsed)
-            if lastUpdateSeen < published:
+            if last_update_seen < published:
                 newEntries.append(entry)
 
         requests.post(
-            "https://chtc-bot.herokuapp.com/rss",
-            headers={'Content-Type': 'application/json', 'X-Shared-Secret': sharedSecret},
-            json=newEntries
+            os.environ["bot_rss_api_endpoint"],
+            headers={
+                "Content-Type": "application/json",
+                "X-Shared-Secret": rss_shared_secret,
+            },
+            json=newEntries,
         )
 
         return {
             "statusCode": 200,
-            "body": json.dumps(f"Feed updated at {lastUpdate}."),
+            "body": json.dumps(f"Feed updated at {last_update}."),
         }
     else:
         return {
             "statusCode": 200,
-            "body": json.dumps(f"No feed updates since {lastUpdate}."),
+            "body": json.dumps(f"No feed updates since {last_update}."),
         }
 
 
