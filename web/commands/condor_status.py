6a8b8bb96c9679562b4cda21a7de985824339f01
Josh Karpel <josh.karpel@gmail.com>
2020-08-27 10:44:06 -0500

    add prototype /condor_status command

---
+++ web/commands/condor_status.py
@@ -0,0 +1,41 @@
+import html
+import shlex
+import subprocess
+
+from flask import current_app, request
+
+from .. import formatting, slack
+from ..executor import executor
+from . import commands
+
+
+class CondorStatusCommandHandler(commands.CommandHandler):
+    def handle(self):
+        channel = request.form.get("channel_id")
+        user = request.form.get("user_id")
+        text = html.unescape(request.form.get("text"))
+
+        executor.submit(condor_status_reply, channel, user, text)
+
+        return f":thinking_face: :card_file_box:", 200
+
+
+def condor_status_reply(channel: str, user: str, text: str):
+    msg = generate_condor_status_reply(user, text)
+
+    slack.post_message(text=msg, channel=channel)
+
+
+def generate_condor_status_reply(user: str, text: str):
+    args = ["condor_status", "-pool", current_app.config.POOL, *shlex.split(text)]
+
+    fmt_args = formatting.fixed(shlex.join(args))
+    current_app.logger.debug(f"About to run: {fmt_args}")
+
+    cmd = subprocess.run(args, text=True, env={})
+
+    if cmd.returncode != 0:
+        current_app.logger.error(f"Error while trying to run {shlex.join(args)}:\n{cmd.stderr}")
+        return f"Error while trying to run {fmt_args} for {user}:\n{formatting.fixed_block(cmd.stderr)}"
+
+    return f"{user} asked me to run {fmt_args}:\n{formatting.fixed_block(cmd.stdout)}"
