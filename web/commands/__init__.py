7ee8b69f127437b1ad81b573ba2ddafd5ae763c6
Josh Karpel <josh.karpel@gmail.com>
2020-08-27 10:59:26 -0500

    actually add the condor_status command handler to config

--- web/commands/__init__.py
+++ web/commands/__init__.py
@@ -1,4 +1,5 @@
 from .classad_eval import ClassadEvalCommandHandler
+from .condor_status import CondorStatusCommandHandler
 from .schedule import ScheduleCommandHandler
 from .scrapers import JobAdsCommandHandler, KnobsCommandHandler, SubmitsCommandHandler
 from .user import UserCommandHandler
